1a4569f57bbd0be56c7b0159a67b008b41961aaf
nahuel2407 <nahuel240701@gmail.com>
2023-09-02 01:32:56 -0300

    agregando las pagina de cada producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,13 +1,24 @@
+
 // Requerimos path para poder enviar los archivos HTML
+const fs = require('fs');
 const path = require("path");
+//vinculamos con el ejs.
+const productsFilePath = path.join(__dirname, '../data/productsdatabase.json');
+
 
 // Creamos el objeto literal con los métodos a exportar
 const productsController = {
 
     // Manejo del pedido get con ruta
     productoDetalle: (req, res) => {
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+	
+		const product = products.find(product => {
+			return product.id == req.params.id
+		});
+		
         // comunicarse con el modelo, conseguir información
-        res.render('../views/producto')
+        res.render('../views/producto', {product})
     },
     productocarga: (req, res) => {
         // comunicarse con el modelo, conseguir información
