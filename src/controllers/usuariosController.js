fd8bc9b343fa15dd02addfe8ceab0ea189ec49bd
nahuel2407 <nahuel240701@gmail.com>
2023-09-20 16:17:40 -0300

    agregando el crear usuario

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,6 +1,17 @@
-// Requerimos path para poder enviar los archivos HTML
-const path = require("path");
+// Requerimos path para poder enviar los archivos HTM
 
+
+
+const { log } = require('console');
+const fs = require('fs');
+const path = require("path");
+//vinculamos con el ejs.
+
+
+
+/* En la constante "products" ya tienen los productos que están 
+guardados en la carpeta date como Json (un array de objetos literales) */
+const usuariosFilePath = path.join(__dirname, '../data/usuariosdatabase.json');
 // Creamos el objeto literal con los métodos a exportar
 const usuariosController = {
 
@@ -13,6 +24,29 @@ const usuariosController = {
         // comunicarse con el modelo, conseguir información
         res.render( "../views/register")
     },
+    prossregistro: (req, res) => {
+        const data = req.body;
+        //leer el archivo json y dejarlo en una variable (array)
+        const usuario = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
+        //crear un nuevo objeto literal con los datos ingresados por el usuario
+        const nuevousuario = {
+            id: usuario[usuario.length - 1].id + 1,
+            name: data.name,
+            apell: data.apell,
+           correo: data.correo,
+           password: data.password,
+            imagen: req.file ? req.file.filename : "default-imagen.png",
+            categoria: data.categoria
+        }
+        //agregar ese objeto al array
+       usuario.push(nuevousuario);
+        //volver a escribir sobre el archivo json 
+        fs.writeFileSync(usuariosFilePath, JSON.stringify(usuario, null, " "))
+        //devolverle alguna vista al usuario
+        res.redirect("/")
+        console.log(nuevousuario)
+    },
+  
 }
 
 // Exportamos el objeto literal con los distintos métodos, que se usará en el enrutador por defecto
--- src/routers/usuariosRouter.js
+++ src/routers/usuariosRouter.js
@@ -2,18 +2,34 @@
 const express = require("express");
 // Router con R mayuscula
 const router = express.Router();
+const multer = require('multer');
+const path = require('path')
 
 // Importamos el controlador de las rutas por defecto
 const usuariosController = require("../controllers/usuariosController.js")
 
 // En vez de app.get, utilizamos router.get. Esto va "guardando" en router las distintas rutas, que luego exportamos
+//********** MULTER CONFIGURACION */
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/img/imagenes-usuario")
+    },
+    filename: function (req, file, cb) {
+        cb(null, "usuario-" + Date.now() + path.extname(file.originalname))
+        //        product-542563573.jpg
+    },
+});
+const upload = multer({storage:storage});
 // Procesa el pedido get con ruta /
 
 
 router.get("/inicioDeSesion",  usuariosController.inicioDeSesion);
 
 router.get("/registro",  usuariosController.registro);
+router.post("/registro", upload.single("imagen"), usuariosController.prossregistro);
+
+
 
 // Exportamos la variable router ya con todas las rutas "guardadas", que se usará en app.js
 module.exports = router;
