86ae3dd0e7b5fb57c5d97668c0bf8ee34321f2ea
nahuel2407 <nahuel240701@gmail.com>
2023-10-31 11:55:21 -0300

    agregue config

---
+++ src/data/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": '',
+      "database": "dbtienda",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": "",
+      "database": "dbtienda",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": "ivan4912134",
+      "database": "dbtienda",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
